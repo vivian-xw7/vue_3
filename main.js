37d6dd4e1bf53c553d7e6ef7568c7b9f0841e5d4
Vivian Hayes <vivian.xw7@gmail.com>
2023-11-18 15:44:07 -0600

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'the thing between your foot and the shoe',
+            image: './assets/images/socks_green.jpg',
+            inventory: 10,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {
+                    id: 1254,
+                    color: 'green',
+                }, 
+                {
+                    id: 1255,
+                    color: 'blue'
+                }
+            ],
+            sizes: ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl'],
+        }
+    }
+})
