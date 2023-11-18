46d0cb5e92f70a748352bdd2a1c6c84e7083e02d
Vivian Hayes <vivian.xw7@gmail.com>
2023-11-18 16:33:20 -0600

    event handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'the thing between your foot and the shoe',
             image: './assets/images/socks_green.jpg',
@@ -11,13 +12,26 @@ const app = Vue.createApp({
                 {
                     id: 1254,
                     color: 'green',
+                    image: './assets/images/socks_green.jpg',
                 }, 
                 {
                     id: 1255,
-                    color: 'blue'
+                    color: 'blue',
+                    image: './assets/images/socks_blue.jpg',
                 }
             ],
             sizes: ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl'],
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            this.cart -= 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
