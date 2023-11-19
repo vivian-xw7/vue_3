fcef8f36f658a25cb80c4e1315e3e9aae8482b2c
Vivian Hayes <vivian.xw7@gmail.com>
2023-11-18 23:58:27 -0600

    computed properrties

--- main.js
+++ main.js
@@ -3,24 +3,23 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            description: 'the thing between your foot and the shoe',
-            image: './assets/images/socks_green.jpg',
-            inventory: 0,
+            brand: 'VIVING DEAD GIRL',
+            selectedVariant: 0,
             onSale: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 {
                     id: 1254,
                     color: 'green',
                     image: './assets/images/socks_green.jpg',
+                    quantity: 0,
                 }, 
                 {
                     id: 1255,
                     color: 'blue',
                     image: './assets/images/socks_blue.jpg',
+                    quantity: 50,
                 }
             ],
-            sizes: ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl'],
         }
     },
     methods: {
@@ -30,8 +29,19 @@ const app = Vue.createApp({
         removeFromCart() {
             this.cart -= 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
+        updateVariant(index) {
+            this.selectedVariant = index
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
     }
 })
